2277b859a910b50ca0c246c5f12ef2b845f56087
Greg Swindle <greg@swindle.net>
2018-04-08 21:42:21 -0500

    ci(travis): clarify release step
    GH-20

--- .github/config/markdown.config.js
+++ .github/config/markdown.config.js
@@ -2,21 +2,24 @@
 /* eslint node/no-unpublished-require: "off" */
 const markdownMagic = require('markdown-magic')
 const markdownMagicDependencyTable = require('markdown-magic-dependency-table')
-const markdownMagicEngines = require('markdown-magic-engines')
 const markdownMagicPackageScripts = require('markdown-magic-package-scripts')
 const path = require('path')
 
 const config = {
-  'DEBUG': false,
-  'transforms': {
-    'DEPENDENCYTABLE': markdownMagicDependencyTable,
-    'ENGINES': markdownMagicEngines,
-    'SCRIPTS': markdownMagicPackageScripts
+  DEBUG: false,
+  transforms: {
+    DEPENDENCYTABLE: markdownMagicDependencyTable,
+    SCRIPTS: markdownMagicPackageScripts
   }
 }
 
-let markdownPath = path.join(__dirname, '**/*.md')
-markdownMagic(markdownPath, config)
+const main = () => {
+  let markdownPath = path.join(__dirname, '**/*.md')
+  markdownMagic(markdownPath, config)
 
-markdownPath = path.join(__dirname, '../*.md')
-markdownMagic(markdownPath, config)
+  markdownPath = path.join(__dirname, '..', 'CONTRIBUTING.md')
+  console.log(markdownPath)
+  markdownMagic(markdownPath, config)
+}
+
+main()
